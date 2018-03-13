d7b3ab78b421cc6b50ae079b4fc5de7e5369f949
rsobko <37337359+rsobko@users.noreply.github.com>
2018-03-13 15:45:47 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default"%>  
+
+<!DOCTYPE html>  
+ 
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head id="Head1" runat="server">    
+    <title>ASP_Test</title>    
+</head>    
+<body>       
+    <form id="form1" runat="server">    
+    <div>    
+       <h>Load Data</h>
+       <asp:Button ID="ButtonLoad" runat="server" Text="Load" OnClick="ButtonLoad_Click" /> 
+        <br></br>
+    </div> 
+    <div>  
+        <asp:SqlDataSource ID="Task_Source" runat="server"
+             ConnectionString="Data Source=localhost;Initial Catalog=ASP_Test;Integrated Security=True;" providerName="System.Data.SqlClient"
+             SelectCommand="SELECT TaskNumber, TaskSubject, TaskPriority, TaskStatus FROM tasks order by TaskNumber" />
+        
+        <asp:GridView ID="GridView1" runat="server"
+            DataSourceID="Task_Source" PageSize="5" AllowPaging="True" AllowSorting="true" EnablePersistedSelection="true" DataKeyNames="TaskNumber" AutoGenerateColumns="False" HorizontalAlign="Center" >
+            <Columns>
+                <asp:BoundField DataField="TaskNumber" HeaderText="TaskNumber" SortExpression="TaskNumber" />
+                <asp:BoundField DataField="TaskSubject" HeaderText="TaskSubject" SortExpression="TaskSubject" />
+                <asp:BoundField DataField="TaskPriority" HeaderText="TaskPriority" SortExpression="TaskPriority" />
+                <asp:BoundField DataField="TaskStatus" HeaderText="TaskStatus" SortExpression="TaskStatus" />
+            </Columns>
+        </asp:GridView>                
+      </div>  
+    </form>   
+</body>    
+</html>  
---
+++ Default.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;  
+using System.IO;  
+using System.Data;  
+using System.Data.SqlClient;  
+using System.Configuration;  
+public partial class _Default : System.Web.UI.Page  
+{  
+     
+    SqlConnection conn;  
+  
+    string sqlconn;  
+    protected void Page_Load(object sender, EventArgs e)  
+    {  
+    
+    }    
+     
+    private void connection()  
+    {  
+        sqlconn = ConfigurationManager.ConnectionStrings["SqlConn"].ConnectionString;  
+        conn = new SqlConnection(sqlconn);  
+  
+    }  
+    
+    protected void ButtonLoad_Click(object sender, EventArgs e)  
+    {            
+        DataTable dt = new DataTable(); 
+
+        dt.Columns.Add("TaskNumber");  
+        dt.Columns.Add("TaskSubject");  
+        dt.Columns.Add("TaskPriority");  
+        dt.Columns.Add("TaskStatus");  
+
+        string CSV_Path = Path.GetFullPath("C:/test/tasks.csv");  
+        string Read_CSV = File.ReadAllText(CSV_Path);  
+
+        foreach (string CSV_Row in Read_CSV.Split('\n'))  
+        {  
+            if (!string.IsNullOrEmpty(CSV_Row))  
+            {  
+                //Adding each row into datatable  
+                dt.Rows.Add();  
+                int count = 0;  
+                foreach (string CSV_Field in CSV_Row.Split(','))  
+                {  
+                    dt.Rows[dt.Rows.Count - 1][count] = CSV_Field;  
+                    count++;  
+                }  
+            }  
+             
+            
+        } 
+        InsertCSVRecords(dt);  
+    }  
+   
+
+    private void InsertCSVRecords(DataTable dt)  
+    {    
+        connection();  
+        conn.Open(); 
+
+        string sqlExpression = String.Format("truncate table tasks");
+        SqlCommand command = new SqlCommand(sqlExpression, conn);
+        command.ExecuteNonQuery();
+   
+        SqlBulkCopy objbulk = new SqlBulkCopy(conn);  
+        objbulk.DestinationTableName = "tasks";  
+        objbulk.ColumnMappings.Add("TaskNumber", "TaskNumber");  
+        objbulk.ColumnMappings.Add("TaskSubject", "TaskSubject");  
+        objbulk.ColumnMappings.Add("TaskPriority", "TaskPriority");  
+        objbulk.ColumnMappings.Add("TaskStatus", "TaskStatus");         
+        objbulk.WriteToServer(dt);  
+
+        GridView1.DataBind();
+        conn.Close();    
+    } 
+}  
